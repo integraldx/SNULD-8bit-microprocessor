32f16411b5a6f74f4c5cf8fce504b830f1a9da0c
Integraldx <jmh121463@gmail.com>
2019-06-06 16:45:05 +0900

    alu

---
+++ alu.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ps
+
+module alu
+(
+    input negate,
+    input [7:0] val_a,
+    input [7:0] val_b,
+    output [7:0] val_e
+) 
+    assign val_e = negate ? val_a - val_b : val_a + val_b;
+endmodule
